0b3f5bd1146e28857dad6621aabf1362f84ca810
Eric Oak <erkoak@gmail.com>
2020-04-30 09:57:17 -0400

    Add files via upload

---
+++ cold-turkey.sql
@@ -0,0 +1,54 @@
+
+-- Retrieve Panjiva U.S. shipment data associated with Russell 3000 companies:
+
+WITH r3k AS (
+SELECT DISTINCT cs.companyid, si.simpleIndustryDescription
+FROM ciqindexconstituent cic
+JOIN ciqtradingitem cti
+ON cic.tradingitemid = cti.tradingitemid
+JOIN ciqsecurity cs
+ON cs.securityid = cti.securityid
+JOIN ciqcompany co ON co.companyid = cs.companyid
+JOIN ciqSimpleIndustry si ON si.simpleIndustryId = co.simpleIndustryId
+WHERE indexid = 2668795 --russell3
+AND cic.todate is NULL
+), imports AS (
+SELECT * FROM XFL_PANJIVA.dbo.panjivaUSImport2016
+UNION ALL
+SELECT * FROM XFL_PANJIVA.dbo.panjivaUSImport2017
+UNION ALL
+SELECT * FROM XFL_PANJIVA.dbo.panjivaUSImport2018
+UNION ALL
+SELECT * FROM XFL_PANJIVA.dbo.panjivaUSImport2019
+UNION ALL
+SELECT * FROM XFL_PANJIVA.dbo.panjivaUSImport2020
+)
+SELECT datepart(year, arrivalDate) AS yr, datepart(month, arrivalDate) AS mo,
+sum(volumeTEU) AS teu, r3k.companyid, r3k.simpleIndustryDescription
+FROM imports
+JOIN XFL_PANJIVA.dbo.panjivaCompanyCrossRef ccr
+ON ccr.identifierValue = imports.conPanjivaId
+JOIN ciqcompanyultimateparent ult
+ON ult.companyId = ccr.companyId
+JOIN r3k
+ON r3k.companyid = ult.companyId
+GROUP BY datepart(year, arrivalDate), datepart(month, arrivalDate),
+r3k.companyid, r3k.simpleIndustryDescription
+
+-- Retrieve Russell 3000 Guidance:
+
+SELECT situation, announcedDate, keydevEventTypeName, co.companyId,
+companyName, country, si.simpleIndustryDescription, kd.keyDevId
+FROM ciqKeyDevToObjectToEventType kdo
+JOIN ciqKeyDev kd
+ON kd.keyDevId = kdo.keyDevId
+JOIN ciqKeyDevObjectRoleType kdr
+ON kdr.keyDevToObjectRoleTypeId = kdo.keyDevToObjectRoleTypeId
+JOIN ciqKeyDevEventType kde ON kde.keyDevEventTypeId = kdo.keyDevEventTypeId
+JOIN ciqCompany co ON co.companyId = kdo.objectId
+JOIN ciqCountryGeo cty ON cty.countryId = co.countryId
+JOIN ciqSimpleIndustry si ON si.simpleIndustryId = co.simpleIndustryId
+WHERE kde.keyDevEventTypeId IN (26, 27, 29, 49, 225)
+AND announcedDate >= '2018-01-01'
+
+
